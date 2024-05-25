587bc7d36a87a7bc79ff0df3e5d0f8817a66c0fa
4sdf32d <128713205+4sdf32d@users.noreply.github.com>
2024-05-25 11:22:45 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,397 @@
+import os
+import datetime
+
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from nets.unet_training import get_lr_scheduler, set_optimizer_lr, weights_init
+from utils.callbacks import LossHistory, EvalCallback
+from utils.dataloader import UnetDataset, unet_dataset_collate
+from utils.utils import download_weights, show_config
+from utils.utils_fit import fit_one_epoch
+# 普通模型
+from nets.unet import ResU_Net,U_Net,SegNet,UNet_2Plus,U2NET,AttU_Net,Dense_Unet,LinkNet
+# 坐标注意力机制+unet
+#from nets.unet_att import UNet_att
+# unet+复杂模块
+from nets.unet_model import UNet_DeConvblock,UNet_GSConvjilian,UNet_MinAspp
+# 混合模型
+from nets.unetplus import UNet_att_aspp,UNet_att_DeConvblock,UNet_att_GSConvjilian,ResUnet_att_aspp
+
+
+if __name__ == "__main__":
+    #---------------------------------#
+    #   Cuda    是否使用Cuda
+    #           没有GPU可以设置成False
+    #---------------------------------#
+    Cuda = True
+    #---------------------------------------------------------------------#
+    #   distributed     用于指定是否使用单机多卡分布式运行
+    #                   终端指令仅支持Ubuntu。CUDA_VISIBLE_DEVICES用于在Ubuntu下指定显卡。
+    #                   Windows系统下默认使用DP模式调用所有显卡，不支持DDP。
+    #   DP模式：
+    #       设置            distributed = False
+    #       在终端中输入    CUDA_VISIBLE_DEVICES=0,1 python train.py
+    #   DDP模式：
+    #       设置            distributed = True
+    #       在终端中输入    CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 train.py
+    #---------------------------------------------------------------------#
+    distributed     = False
+    #---------------------------------------------------------------------#
+    #   sync_bn     是否使用sync_bn，DDP模式多卡可用
+    #---------------------------------------------------------------------#
+    sync_bn         = False
+    #---------------------------------------------------------------------#
+    #   fp16        是否使用混合精度训练
+    #               可减少约一半的显存、需要pytorch1.7.1以上
+    #---------------------------------------------------------------------#
+    fp16            = False
+    #-----------------------------------------------------#
+    #   num_classes     训练自己的数据集必须要修改的
+    #                   自己需要的分类个数+1，如2+1
+    #-----------------------------------------------------#
+    num_classes = 2
+    backbone    = ""
+    pretrained  = False
+    model_path  = ""
+    #   input_shape     输入图片的大小，32的倍数
+    input_shape = [512, 512]
+    # 这个没有
+    Init_Epoch          = 0
+    Freeze_Epoch        = 10
+    Freeze_batch_size   = 2
+    # 这个再用
+    UnFreeze_Epoch      = 10
+    Unfreeze_batch_size = 1
+    Freeze_Train        = False
+    #   Init_lr         模型的最大学习率
+    #                   当使用Adam优化器时建议设置  Init_lr=1e-4
+    #                   当使用SGD优化器时建议设置   Init_lr=1e-2
+    #   Min_lr          模型的最小学习率，默认为最大学习率的0.01
+    Init_lr             = 1e-4
+    Min_lr              = Init_lr * 0.01
+    #   optimizer_type  使用到的优化器种类，可选的有adam、sgd
+    #                   当使用Adam优化器时建议设置  Init_lr=1e-4
+    #                   当使用SGD优化器时建议设置   Init_lr=1e-2
+    #   momentum        优化器内部使用到的momentum参数
+    #   weight_decay    权值衰减，可防止过拟合
+    #                   adam会导致weight_decay错误，使用adam时建议设置为0。
+    optimizer_type      = "adam"
+    momentum            = 0.9
+    weight_decay        = 0
+    #   lr_decay_type   使用到的学习率下降方式，可选的有'step'、'cos'
+    lr_decay_type       = 'cos'
+    #   save_period     多少个epoch保存一次权值
+    save_period         = 5
+    #   save_dir        权值与日志文件保存的文件夹
+    save_dir            = 'logs'
+    #   eval_flag       是否在训练时进行评估，评估对象为验证集
+    #   eval_period     代表多少个epoch评估一次，不建议频繁的评估
+    #                   评估需要消耗较多的时间，频繁评估会导致训练非常慢
+    #   此处获得的mAP会与get_map.py获得的会有所不同，原因有二：
+    #   （一）此处获得的mAP为验证集的mAP。
+    #   （二）此处设置评估参数较为保守，目的是加快评估速度。
+    eval_flag           = True
+    eval_period         = 5
+
+    #   数据集路径
+    VOCdevkit_path  = 'VOCdevkit'
+    #   建议选项：
+    #   种类少（几类）时，设置为True
+    #   种类多（十几类）时，如果batch_size比较大（10以上），那么设置为True
+    #   种类多（十几类）时，如果batch_size比较小（10以下），那么设置为False
+    dice_loss       = False
+    #   是否使用focal loss来防止正负样本不平衡
+    focal_loss      = False
+    #------------------------------------------------------------------#
+    #   是否给不同种类赋予不同的损失权值，默认是平衡的。
+    #   设置的话，注意设置成numpy形式的，长度和num_classes一样。
+    #   如：
+    #   num_classes = 3
+    #   cls_weights = np.array([1, 2, 3], np.float32)
+    #------------------------------------------------------------------#
+    cls_weights     = np.ones([num_classes], np.float32)
+    #------------------------------------------------------------------#
+    #   num_workers     用于设置是否使用多线程读取数据，1代表关闭多线程
+    #                   开启后会加快数据读取速度，但是会占用更多内存
+    #                   keras里开启多线程有些时候速度反而慢了许多
+    #                   在IO为瓶颈的时候再开启多线程，即GPU运算速度远大于读取图片的速度。
+    #------------------------------------------------------------------#
+    num_workers     = 0
+
+    #------------------------------------------------------#
+    #   设置用到的显卡
+    #------------------------------------------------------#
+    ngpus_per_node  = torch.cuda.device_count()
+    if distributed:
+        dist.init_process_group(backend="nccl")
+        local_rank  = int(os.environ["LOCAL_RANK"])
+        rank        = int(os.environ["RANK"])
+        device      = torch.device("cuda", local_rank)
+        if local_rank == 0:
+            print(f"[{os.getpid()}] (rank = {rank}, local_rank = {local_rank}) training...")
+            print("Gpu Device Count : ", ngpus_per_node)
+    else:
+        device          = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        local_rank      = 0
+
+    #----------------------------------------------------#
+    #   下载预训练权重
+    #----------------------------------------------------#
+    if pretrained:
+        if distributed:
+            if local_rank == 0:
+                download_weights(backbone)  
+            dist.barrier()
+        else:
+            download_weights(backbone)
+
+    # 训练模型选择————————用到那个注释开那个！！！！！！！！
+    # model = U_Net(img_ch=3, num_classes=num_classes).train()
+    # 添复杂模块的模型
+    # model = UNet_DeConvblock(img_ch=3, num_classes=num_classes).train()
+    # model = UNet_GSConvjilian(img_ch=3, num_classes=num_classes).train()
+    # model = UNet_MinAspp(img_ch=3, num_classes=num_classes).train()
+
+    # 添注意力机制的模型
+    #model = UNet_att(img_ch=3, num_classes=num_classes).train()
+
+    # 基础模型
+    # model = LinkNet().train()
+    # model = U2NET(in_ch=3, out_ch=num_classes).train()
+    # model = UNet_2Plus(img_ch=3, num_classes=num_classes).train()
+    # model = AttU_Net(img_ch=3, output_ch=num_classes).train()
+    # model = Dense_Unet(in_chan=3,out_chan=num_classes,filters=128, num_conv=4).train()
+    # model = ResU_Net( img_ch=3, num_classes=num_classes).train()
+    # model = SegNet(input_nbr=3, label_nbr=num_classes).train()
+
+    # 混合模型
+    #model = UNet_att_aspp(img_ch=3, num_classes=num_classes).train()
+    model = ResUnet_att_aspp(img_ch=3, num_classes=num_classes).train()
+
+    if not pretrained:
+        weights_init(model)
+    if model_path != '':
+        #------------------------------------------------------#
+        #   权值文件请看README，百度网盘下载
+        #------------------------------------------------------#
+        if local_rank == 0:
+            print('Load weights {}.'.format(model_path))
+        
+        #------------------------------------------------------#
+        #   根据预训练权重的Key和模型的Key进行加载
+        #------------------------------------------------------#
+        model_dict      = model.state_dict()
+        pretrained_dict = torch.load(model_path, map_location = device)
+        load_key, no_load_key, temp_dict = [], [], {}
+        for k, v in pretrained_dict.items():
+            if k in model_dict.keys() and np.shape(model_dict[k]) == np.shape(v):
+                temp_dict[k] = v
+                load_key.append(k)
+            else:
+                no_load_key.append(k)
+        model_dict.update(temp_dict)
+        model.load_state_dict(model_dict)
+        #------------------------------------------------------#
+        #   显示没有匹配上的Key
+        #------------------------------------------------------#
+        if local_rank == 0:
+            print("\nSuccessful Load Key:", str(load_key)[:500], "……\nSuccessful Load Key Num:", len(load_key))
+            print("\nFail To Load Key:", str(no_load_key)[:500], "……\nFail To Load Key num:", len(no_load_key))
+            print("\n\033[1;33;44m温馨提示，head部分没有载入是正常现象，Backbone部分没有载入是错误的。\033[0m")
+
+    #----------------------#
+    #   记录Loss
+    #----------------------#
+    if local_rank == 0:
+        time_str        = datetime.datetime.strftime(datetime.datetime.now(),'%Y_%m_%d_%H_%M_%S')
+        log_dir         = os.path.join(save_dir, "loss_" + str(time_str))
+        loss_history    = LossHistory(log_dir, model, input_shape=input_shape)
+    else:
+        loss_history    = None
+        
+    #------------------------------------------------------------------#
+    #   torch 1.2不支持amp，建议使用torch 1.7.1及以上正确使用fp16
+    #   因此torch1.2这里显示"could not be resolve"
+    #------------------------------------------------------------------#
+    if fp16:
+        from torch.cuda.amp import GradScaler as GradScaler
+        scaler = GradScaler()
+    else:
+        scaler = None
+
+    model_train     = model.train()
+    #----------------------------#
+    #   多卡同步Bn
+    #----------------------------#
+    if sync_bn and ngpus_per_node > 1 and distributed:
+        model_train = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model_train)
+    elif sync_bn:
+        print("Sync_bn is not support in one gpu or not distributed.")
+
+    if Cuda:
+        if distributed:
+            #----------------------------#
+            #   多卡平行运行
+            #----------------------------#
+            model_train = model_train.cuda(local_rank)
+            model_train = torch.nn.parallel.DistributedDataParallel(model_train, device_ids=[local_rank], find_unused_parameters=True)
+        else:
+            model_train = torch.nn.DataParallel(model)
+            cudnn.benchmark = True
+            model_train = model_train.cuda()
+    
+    #---------------------------#
+    #   读取数据集对应的txt
+    #---------------------------#
+    with open(os.path.join(VOCdevkit_path, "VOC2007/ImageSets/Segmentation/train.txt"),"r") as f:
+        train_lines = f.readlines()
+    with open(os.path.join(VOCdevkit_path, "VOC2007/ImageSets/Segmentation/val.txt"),"r") as f:
+        val_lines = f.readlines()
+    num_train   = len(train_lines)
+    num_val     = len(val_lines)
+        
+    if local_rank == 0:
+        show_config(
+            num_classes = num_classes, backbone = backbone, model_path = model_path, input_shape = input_shape, \
+            Init_Epoch = Init_Epoch, Freeze_Epoch = Freeze_Epoch, UnFreeze_Epoch = UnFreeze_Epoch, Freeze_batch_size = Freeze_batch_size, Unfreeze_batch_size = Unfreeze_batch_size, Freeze_Train = Freeze_Train, \
+            Init_lr = Init_lr, Min_lr = Min_lr, optimizer_type = optimizer_type, momentum = momentum, lr_decay_type = lr_decay_type, \
+            save_period = save_period, save_dir = save_dir, num_workers = num_workers, num_train = num_train, num_val = num_val
+        )
+    #------------------------------------------------------#
+    #   主干特征提取网络特征通用，冻结训练可以加快训练速度
+    #   也可以在训练初期防止权值被破坏。
+    #   Init_Epoch为起始世代
+    #   Interval_Epoch为冻结训练的世代
+    #   Epoch总训练世代
+    #   提示OOM或者显存不足请调小Batch_size
+    #------------------------------------------------------#
+    if True:
+        UnFreeze_flag = False
+        #------------------------------------#
+        #   冻结一定部分训练
+        #------------------------------------#
+        if Freeze_Train:
+            model.freeze_backbone()
+            
+        #-------------------------------------------------------------------#
+        #   如果不冻结训练的话，直接设置batch_size为Unfreeze_batch_size
+        #-------------------------------------------------------------------#
+        batch_size = Freeze_batch_size if Freeze_Train else Unfreeze_batch_size
+
+        #-------------------------------------------------------------------#
+        #   判断当前batch_size，自适应调整学习率
+        #-------------------------------------------------------------------#
+        nbs             = 16
+        lr_limit_max    = 1e-4 if optimizer_type == 'adam' else 1e-1
+        lr_limit_min    = 1e-4 if optimizer_type == 'adam' else 5e-4
+        Init_lr_fit     = min(max(batch_size / nbs * Init_lr, lr_limit_min), lr_limit_max)
+        Min_lr_fit      = min(max(batch_size / nbs * Min_lr, lr_limit_min * 1e-2), lr_limit_max * 1e-2)
+
+        #---------------------------------------#
+        #   根据optimizer_type选择优化器
+        #---------------------------------------#
+        optimizer = {
+            'adam'  : optim.Adam(model.parameters(), Init_lr_fit, betas = (momentum, 0.999), weight_decay = weight_decay),
+            'sgd'   : optim.SGD(model.parameters(), Init_lr_fit, momentum = momentum, nesterov=True, weight_decay = weight_decay)
+        }[optimizer_type]
+
+        #---------------------------------------#
+        #   获得学习率下降的公式
+        #---------------------------------------#
+        lr_scheduler_func = get_lr_scheduler(lr_decay_type, Init_lr_fit, Min_lr_fit, UnFreeze_Epoch)
+        
+        #---------------------------------------#
+        #   判断每一个世代的长度
+        #---------------------------------------#
+        epoch_step      = num_train // batch_size
+        epoch_step_val  = num_val // batch_size
+        
+        if epoch_step == 0 or epoch_step_val == 0:
+            raise ValueError("数据集过小，无法继续进行训练，请扩充数据集。")
+
+        train_dataset   = UnetDataset(train_lines, input_shape, num_classes, True, VOCdevkit_path)
+        val_dataset     = UnetDataset(val_lines, input_shape, num_classes, False, VOCdevkit_path)
+        
+        if distributed:
+            train_sampler   = torch.utils.data.distributed.DistributedSampler(train_dataset, shuffle=True,)
+            val_sampler     = torch.utils.data.distributed.DistributedSampler(val_dataset, shuffle=False,)
+            batch_size      = batch_size // ngpus_per_node
+            shuffle         = False
+        else:
+            train_sampler   = None
+            val_sampler     = None
+            shuffle         = True
+
+        gen             = DataLoader(train_dataset, shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True,
+                                    drop_last = True, collate_fn = unet_dataset_collate, sampler=train_sampler)
+        gen_val         = DataLoader(val_dataset  , shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True, 
+                                    drop_last = True, collate_fn = unet_dataset_collate, sampler=val_sampler)
+        
+        #----------------------#
+        #   记录eval的map曲线
+        #----------------------#
+        if local_rank == 0:
+            eval_callback   = EvalCallback(model, input_shape, num_classes, val_lines, VOCdevkit_path, log_dir, Cuda, \
+                                            eval_flag=eval_flag, period=eval_period)
+        else:
+            eval_callback   = None
+        
+        #---------------------------------------#
+        #   开始模型训练
+        #---------------------------------------#
+        for epoch in range(Init_Epoch, UnFreeze_Epoch):
+            #---------------------------------------#
+            #   如果模型有冻结学习部分
+            #   则解冻，并设置参数
+            #---------------------------------------#
+            if epoch >= Freeze_Epoch and not UnFreeze_flag and Freeze_Train:
+                batch_size = Unfreeze_batch_size
+
+                #-------------------------------------------------------------------#
+                #   判断当前batch_size，自适应调整学习率
+                #-------------------------------------------------------------------#
+                nbs             = 16
+                lr_limit_max    = 1e-4 if optimizer_type == 'adam' else 1e-1
+                lr_limit_min    = 1e-4 if optimizer_type == 'adam' else 5e-4
+                Init_lr_fit     = min(max(batch_size / nbs * Init_lr, lr_limit_min), lr_limit_max)
+                Min_lr_fit      = min(max(batch_size / nbs * Min_lr, lr_limit_min * 1e-2), lr_limit_max * 1e-2)
+                #---------------------------------------#
+                #   获得学习率下降的公式
+                #---------------------------------------#
+                lr_scheduler_func = get_lr_scheduler(lr_decay_type, Init_lr_fit, Min_lr_fit, UnFreeze_Epoch)
+                    
+                model.unfreeze_backbone()
+                            
+                epoch_step      = num_train // batch_size
+                epoch_step_val  = num_val // batch_size
+
+                if epoch_step == 0 or epoch_step_val == 0:
+                    raise ValueError("数据集过小，无法继续进行训练，请扩充数据集。")
+
+                if distributed:
+                    batch_size = batch_size // ngpus_per_node
+       
+                gen             = DataLoader(train_dataset, shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True,
+                                            drop_last = True, collate_fn = unet_dataset_collate, sampler=train_sampler)
+                gen_val         = DataLoader(val_dataset  , shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True, 
+                                            drop_last = True, collate_fn = unet_dataset_collate, sampler=val_sampler)
+
+                UnFreeze_flag = True
+
+            if distributed:
+                train_sampler.set_epoch(epoch)
+
+            set_optimizer_lr(optimizer, lr_scheduler_func, epoch)
+
+            fit_one_epoch(model_train, model, loss_history, eval_callback, optimizer, epoch, 
+                    epoch_step, epoch_step_val, gen, gen_val, UnFreeze_Epoch, Cuda, dice_loss, focal_loss, cls_weights, num_classes, fp16, scaler, save_period, save_dir, local_rank)
+
+            if distributed:
+                dist.barrier()
+
+        if local_rank == 0:
+            loss_history.writer.close()
